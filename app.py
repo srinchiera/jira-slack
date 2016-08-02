b1077f93d3550ae1678fcdcda44c19729dde0aeb
Salvatore Rinchiera <salvatore@rinchiera.com>
2016-08-02 13:13:26 -0400

    updateS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from jira import JIRA
+from jira_api import JiraApi
 
 from config import (
     JIRA_URL,
@@ -6,9 +7,25 @@ from config import (
     PASSWORD,
 )
 
-jira = JIRA(JIRA_URL, basic_auth=(USERNAME, PASSWORD))
+jira_client = JIRA(JIRA_URL, basic_auth=(USERNAME, PASSWORD))
+jira_api = JiraApi(jira_client)
 
-issue = jira.issue('OIQ-674')
-print issue.fields.project.key             # 'JRA'
-print issue.fields.issuetype.name          # 'New Feature'
-print issue.fields.reporter.displayName    # 'Mike Cannon-Brookes [Atlassian]'
+'''
+jira_api.create(project='OIQ',
+                summary='this is a test',
+                issue_type='Bug',
+                assignee='srinchiera',
+                reporter='srinchiera',
+                description='wow so cool!')
+'''
+
+'''
+jira_api.modify('OIQ-683', assignee='jhogan', status='Reopen')
+'''
+
+jira_api.show('OIQ-683', assignee='jhogan')
+
+#issue = jira.issue('OIQ-674')
+#print issue.fields.project.key             # 'JRA'
+#print issue.fields.issuetype.name          # 'New Feature'
+#print issue.fields.reporter.displayName    # 'Mike Cannon-Brookes [Atlassian]'
--- jira_api.py
+++ jira_api.py
@@ -1,20 +1,65 @@
 from jira import JIRA
 
-def create(jira, project, assignee, reporter, title, description):
-    """Creates a JIRA issue with parameters"""
+class JiraApi(object):
 
-    new_issue = jira.create_issue(project='PROJ_key_or_id',
-                                  summary='New issue from jira-python',
-                                  description='Look into this one',
-                                  issuetype={'name': 'Bug'})
-    pass
+    def __init__(self, client):
+        self.client = client
 
 
-def show(jira, project, issue_type, username):
-    """Returns all open JIRA issues for issue_type and username"""
-    pass
+    def create(self, project, summary, issue_type,
+               assignee=None, reporter=None, description=None):
+        """Creates a JIRA issue with parameters"""
+
+        issue_params = {
+            'project': {'key': project},
+            'summary': summary,
+            'issuetype': {'name': issue_type}
+        }
+
+        if assignee:
+            issue_params['assignee'] = {'name': assignee}
+
+        if reporter:
+            issue_params['reporter'] = {'name': reporter}
+
+        if description:
+            issue_params['description'] = description
 
 
-def modify(jira, issue, reporter, status):
-    """Closes JIRA issue"""
-    pass
+        new_issue = self.client.create_issue(fields=issue_params)
+
+
+    def show(self, project, issue_type=None, assignee=None):
+        """Returns all open JIRA issues for issue_type and username"""
+        project = self.client.project('OIQ')
+        print project.__dict__
+        jira.search_issues('project=PROJ')
+
+
+    def modify(self, issue, assignee=None, reporter=None, status=None):
+        """Closes JIRA issue"""
+
+        if status:
+            transitions = self.client.transitions(issue)
+            transition_id = None
+            for transition in transitions:
+                transition_name = transition['name'].strip()
+                if status == transition_name:
+                    transition_id = transition['id']
+                    break
+
+            if transition_id is None:
+                print "Error!"
+                return
+
+            self.client.transition_issue(issue, transition_id)
+
+        issue_params = {}
+        if assignee:
+            issue_params['assignee'] = {'name': assignee}
+        if reporter:
+            issue_params['assignee'] = {'name': assignee}
+
+        if issue_params:
+            issue = self.client.issue(issue)
+            issue.update(fields=issue_params)
